fb5a02c07aaf3ef6ca2567565885e99f9e751390
domin <dominika.mmatyka@gmail.com>
2024-06-18 17:24:23 +0200

    fix side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -11,19 +11,27 @@ const Sidebar = () => {
     };
   return (
     <>
-<button onClick={toggleSidebar} aria-controls="logo-sidebar" type="button" className={`inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 ${isSidebarOpen ? 'fixed top-2 left-2 z-50' : ''}`}>  
-  <span className="sr-only">Open sidebar</span>
+ <button onClick={toggleSidebar} aria-controls="logo-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+        <span className="sr-only">Open sidebar</span>
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
       </button>
 
-      <aside id="logo-sidebar" className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0`} aria-label="Sidebar">
-         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800" onClick={toggleSidebar}>
-            <Link to="/" className="flex items-center ps-2.5 mb-5">
+<aside id="logo-sidebar" className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} sm:translate-x-0`} aria-label="Sidebar">
+         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+         <div>
+         {isSidebarOpen && (
+            <button onClick={toggleSidebar} className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-1.5">
+               <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+               </svg>
+            </button>)}
+            <Link to="/" className="flex items-center ps-2.5 mb-5 mt-3">
                <StockManagementLogo className="h-6 me-3 sm:h-7" alt="Stock Management Logo"/>
                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Stock Management</span>
             </Link>
+            </div>
           <ul className="space-y-2 font-medium">
             <li>
               <Link to="/warehouses" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
